7df7de511c4ad81de71460e4c6302e60096ea7a3
sephoraalaye <alayesephora99@gmail.com>
2025-06-20 23:55:30 +0200

    dfghjhgfd qsdfghfds

--- task04/push_that.sh
+++ task04/push_that.sh
@@ -3,10 +3,13 @@
 
 git add .
 
-if [ $# = 1 ]
-then
-    git commit -m "$1"
-    git push -u origin main
-else
-    echo "Entrez un message"
-fi
+while [ $# != 1 ]
+do 
+   echo "Entrez un message"
+done
+
+git add .
+git commit -m "$1"
+git push -u origin main
+
+
